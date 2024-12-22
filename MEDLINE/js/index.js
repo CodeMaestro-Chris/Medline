89eb59e71e01e2501e54dd7cec558f6f9060b0f5
CodeMaestro_Chris <meshackchristian0@gmail.com>
2024-12-22 23:00:44 +0100

    to-the-top button added and animated

--- MEDLINE/js/index.js
+++ MEDLINE/js/index.js
@@ -26,10 +26,18 @@ function send() {
 //The rolling effect on the darkmode button
 document.addEventListener('DOMContentLoaded', () => {
     const darkModeButton = document.querySelector('.dark');
+    const goTotop = document.querySelector('.to-the-top');
+
+    goTotop.style.position = 'fixed';
+    goTotop.style.bottom = '30px';
+    goTotop.style.right = '20px'; // Initially hidden
+    goTotop.style.transition = 'transform 1s ease, top 1s ease';
+
+    // Make the darkmode button visible and positioned
     darkModeButton.style.position = 'fixed';
     darkModeButton.style.bottom = '20px';
     darkModeButton.style.right = '-100px'; // Initially hidden
-    darkModeButton.style.transition = 'transform 1s ease, right 1s ease';
+    darkModeButton.style.transition = 'transform 1.1s ease, right 1.1s ease';
 
     darkModeButton.addEventListener('click', toggleDarkMode);
 
@@ -44,9 +52,13 @@ document.addEventListener('DOMContentLoaded', () => {
         if (window.scrollY > scrollThreshold) {
             darkModeButton.style.right = '20px'; // Move in
             darkModeButton.style.transform = 'rotate(-360deg)'; // Rotate while moving out
+            goTotop.style.transform = 'translateY(-50px)'; // Move up to top
+            goTotop.style.opacity = '1'
         } else {
             darkModeButton.style.right = '-50px'; // Move out
             darkModeButton.style.transform = 'rotate(0deg)'; // Stop rotation
+            goTotop.style.opacity = '0'; // Reset to original position
+            goTotop.style.transform = 'translateY(0px)'; // Reset to original position
         }
     });
 
